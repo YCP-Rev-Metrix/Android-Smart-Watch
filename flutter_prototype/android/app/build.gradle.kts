8ed526c2f89e45c6bb4b7f50633cb33e79d1175e
Charles Carroll <158611220+CharlesBCarroll@users.noreply.github.com>
2026-02-11 13:17:43 -0500

    gradle

--- flutter_prototype/android/app/build.gradle.kts
+++ flutter_prototype/android/app/build.gradle.kts
@@ -11,14 +11,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         // Required for some plugins that use newer Java APIs (core library desugaring)
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
